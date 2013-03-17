c54214477f2ded4aaf464df1c5bb83021172eb3f
Michael Zanetti <michael.zanetti@canonical.com>
2013-03-17 22:36:27 +0100

    fix autor in copyright headers

--- tests/autopilot/testapp/testapp.cpp
+++ tests/autopilot/testapp/testapp.cpp
@@ -2,7 +2,7 @@
  * Copyright (C) 2013 Canonical, Ltd.
  *
  * Authors:
- *  Nicolas d'Offay <nicolas.doffay@canonical.com>
+ *  Michael Zanetti <michael.zanetti@canonical.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
