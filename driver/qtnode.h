2a9d1be4a085e905359c5121e5e36b8ef92ad021
Christopher Lee <chris.lee@canonical.com>
2014-06-12 15:41:09 +1200

    Initial addition of the required interfaces and classes.

--- driver/qtnode.h
+++ driver/qtnode.h
@@ -6,6 +6,10 @@
 #include <QDBusArgument>
 #include <xpathselect/node.h>
 
+#include <QModelIndex>
+class QTableWidgetItem;
+class QStandardItem;
+
 /// A simple data structure representing the state of a single node:
 struct NodeIntrospectionData
 {
@@ -18,6 +22,129 @@ Q_DECLARE_METATYPE(QList<NodeIntrospectionData>);
 
 QDBusArgument &operator<<(QDBusArgument &argument, const NodeIntrospectionData &node_data);
 const QDBusArgument &operator>>(const QDBusArgument &argument, NodeIntrospectionData &node_data);
+
+// Interface for Introspecting an object to query it's details.
+class DBusNode : public xpathselect::Node
+{
+public:
+    typedef std::shared_ptr<const DBusNode> Ptr;
+
+    DBusNode() {}
+    virtual ~DBusNode() {}
+
+    virtual NodeIntrospectionData GetIntrospectionData() const=0;
+
+private:
+    std::string full_path_;
+    Ptr parent_;
+};
+
+/// Specialist class for all QObject object nodes.
+/// This will cover a majority of what we use and we will only need to break
+/// out to specilist classes for a couple of minor (i.e. QModelIndex)
+///
+/// QObjectNode wraps a single QObject pointer. It derives from
+/// xpathselect::Node (DBusNode) and, like that class, is designed to be
+/// allocated on the heap and stored in a std::shared_ptr.
+class QObjectNode : public DBusNode
+{
+public:
+    QObjectNode(QObject* object, Ptr parent);
+    explicit QObjectNode(QObject* object);
+
+    QObject* getWrappedObject() const;
+
+    // DBusNode
+    virtual NodeIntrospectionData GetIntrospectionData() const;
+
+    // xpathselect::Node
+    xpathselect::Node::Ptr GetParent() const;
+    virtual std::string GetName() const;
+    virtual std::string GetPath() const;
+    virtual int32_t GetId() const;
+    virtual bool MatchStringProperty(const std::string& name, const std::string& value) const;
+    virtual bool MatchIntegerProperty(const std::string& name, int32_t value) const;
+    virtual bool MatchBooleanProperty(const std::string& name, bool value) const;
+    virtual xpathselect::NodeVector Children() const;
+
+private:
+    QObject *object_;
+};
+
+class QModelIndexNode : public DBusNode
+{
+public:
+    QModelIndexNode(QModelIndex index, Ptr parent);
+    explicit QModelIndexNode(QModelIndex index);
+
+    QModelIndex getWrappedObject() const;
+
+    // DBusNode
+    virtual NodeIntrospectionData GetIntrospectionData() const;
+
+    // xpathselect::Node
+    xpathselect::Node::Ptr GetParent() const;
+    virtual std::string GetName() const;
+    virtual std::string GetPath() const;
+    virtual int32_t GetId() const;
+    virtual bool MatchStringProperty(const std::string& name, const std::string& value) const;
+    virtual bool MatchIntegerProperty(const std::string& name, int32_t value) const;
+    virtual bool MatchBooleanProperty(const std::string& name, bool value) const;
+    virtual xpathselect::NodeVector Children() const;
+
+private:
+    QModelIndex index_;
+};
+
+class QStandardItemNode : public DBusNode
+{
+public:
+    QStandardItemNode(QStandardItem *item, Ptr parent);
+    explicit QStandardItemNode(QStandardItem *item);
+
+    QStandardItem* getWrappedObject() const;
+
+    // DBusNode
+    virtual NodeIntrospectionData GetIntrospectionData() const;
+
+    // xpathselect::Node
+    xpathselect::Node::Ptr GetParent() const;
+    virtual std::string GetName() const;
+    virtual std::string GetPath() const;
+    virtual int32_t GetId() const;
+    virtual bool MatchStringProperty(const std::string& name, const std::string& value) const;
+    virtual bool MatchIntegerProperty(const std::string& name, int32_t value) const;
+    virtual bool MatchBooleanProperty(const std::string& name, bool value) const;
+    virtual xpathselect::NodeVector Children() const;
+
+private:
+    QStandardItem *item_;
+};
+
+class QTableWidgetItemNode : public DBusNode
+{
+public:
+    QTableWidgetItemNode(QTableWidgetItem *item, Ptr parent);
+    explicit QTableWidgetItemNode(QTableWidgetItem *item);
+
+    QTableWidgetItem* getWrappedObject() const;
+
+    // DBusNode
+    virtual NodeIntrospectionData GetIntrospectionData() const;
+
+    // xpathselect::Node
+    xpathselect::Node::Ptr GetParent() const;
+    virtual std::string GetName() const;
+    virtual std::string GetPath() const;
+    virtual int32_t GetId() const;
+    virtual bool MatchStringProperty(const std::string& name, const std::string& value) const;
+    virtual bool MatchIntegerProperty(const std::string& name, int32_t value) const;
+    virtual bool MatchBooleanProperty(const std::string& name, bool value) const;
+    virtual xpathselect::NodeVector Children() const;
+
+private:
+    QTableWidgetItem *item_;
+};
 
 /// Base class for all Qt-based object nodes.
 ///
