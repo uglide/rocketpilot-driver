7c3e2fddb80ae90b37b427b98ca00f329094c9ad
Christopher Lee <chris.lee@canonical.com>
2014-06-23 11:06:57 +1200

    Adding new interfaces

--- driver/qtnode.h
+++ driver/qtnode.h
@@ -19,6 +19,53 @@ Q_DECLARE_METATYPE(QList<NodeIntrospectionData>);
 QDBusArgument &operator<<(QDBusArgument &argument, const NodeIntrospectionData &node_data);
 const QDBusArgument &operator>>(const QDBusArgument &argument, NodeIntrospectionData &node_data);
 
+// Interface for Introspecting an object to query it's details.
+class DBusNode : public xpathselect::Node
+{
+public:
+    typedef std::shared_ptr<const DBusNode> Ptr;
+
+    DBusNode() {}
+    virtual ~DBusNode() {}
+
+    virtual NodeIntrospectionData GetIntrospectionData() const=0;
+
+private:
+    std::string full_path_;
+    Ptr parent_;
+};
+
+/// Specialist class for all QObject object nodes.
+/// This will cover a majority of what we use and we will only need to break
+/// out to specilist classes for a couple of minor (i.e. QModelIndex)
+///
+/// QObjectNode wraps a single QObject pointer. It derives from
+/// xpathselect::Node (DBusNode) and, like that class, is designed to be
+/// allocated on the heap and stored in a std::shared_ptr.
+class QObjectNode : public DBusNode
+{
+public:
+    QObjectNode(QObject* object, Ptr parent);
+    explicit QObjectNode(QObject* object);
+
+    QObject* getWrappedObject() const;
+
+    // DBusNode
+    virtual NodeIntrospectionData GetIntrospectionData() const;
+
+    // xpathselect::Node
+    xpathselect::Node::Ptr GetParent() const;
+    virtual std::string GetName() const;
+    virtual std::string GetPath() const;
+    virtual int32_t GetId() const;
+    virtual bool MatchStringProperty(const std::string& name, const std::string& value) const;
+    virtual bool MatchIntegerProperty(const std::string& name, int32_t value) const;
+    virtual bool MatchBooleanProperty(const std::string& name, bool value) const;
+    virtual xpathselect::NodeVector Children() const;
+
+private:
+    QObject *object_;
+};
 /// Base class for all Qt-based object nodes.
 ///
 /// QtNode wraps a single QObject pointer. It derives from xpathselect::Node and,
